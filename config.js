d14d4be43c106165dd4c51c2148a45e09d67ef87
Ngọc Tiên Z <99634127+tien2k6z1@users.noreply.github.com>
2023-12-02 15:36:56 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+//====Nhập Thông Tin Của Bạn Vào Đây==============
+
+//Link Ảnh Background
+ImgLink_Background = "https://drive.google.com/uc?export=download&id=1ikIjybvki5H6EE3YtqtRjXpBmGa5kkT4"
+
+//Link Ảnh Avatar
+ImgLink_Avatar = ""
+
+//Nickname
+Nickname_Profile = "ngoctienz"
+
+//Tên Thật
+RealName_Profile = "Ngọc Tiên Z"
+
+//Các Link Của Bạn
+LinkInformation = [
+    Facebook_LinkProfile = "https://www.facebook.com/tien2k6v1",
+    Discord_LinkProfile = "#01",
+    TikTok_LinkProfile = "#02",
+    Instagram_LinkProfile = "#03"
+]
